2abd42e0f58ca2c091ae264b3dcc6f5b6c6c77ea
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-02-11 09:31:41 +0530

    Player movement

---
+++ Rush and Save/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    PlayerController controller;
+
+    [Header("Inputs")]
+    [SerializeField] Vector2 moveInput;
+
+    void Awake()
+    {
+        controller = new PlayerController();
+
+        Movement();
+    }
+
+    void Movement()
+    {
+        controller.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controller.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    void OnEnable()
+    {
+        controller.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        controller.Player.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        Vector2 move = rb.position + moveInput * speed * Time.deltaTime;
+        rb.MovePosition(move);
+    }
+}
