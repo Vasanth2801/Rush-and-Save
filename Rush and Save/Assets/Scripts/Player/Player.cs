b1a54f221948038265c3fd16d2dbfa7b455725c3
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-02-11 12:21:29 +0530

    Enemy Animations

--- Rush and Save/Assets/Scripts/Player/Player.cs
+++ Rush and Save/Assets/Scripts/Player/Player.cs
@@ -15,7 +15,6 @@ public class Player : MonoBehaviour
     void Awake()
     {
         controller = new PlayerController();
-
         Movement();
     }
 
@@ -46,7 +45,7 @@ public class Player : MonoBehaviour
         rb.MovePosition(move);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Resucer"))
         {
